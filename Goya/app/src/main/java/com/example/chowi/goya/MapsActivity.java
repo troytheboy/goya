bf74854d2433a46373364e901f5af1689df23e66
Alex Chow <Alex Chow>
2017-04-15 18:08:48 -0700

    unlinked and not working login and sign up

--- Goya/app/src/main/java/com/example/chowi/goya/MapsActivity.java
+++ Goya/app/src/main/java/com/example/chowi/goya/MapsActivity.java
@@ -179,18 +179,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Write a message to the database
         Log.i("hello", "firebase should have just worked");
-
-        // Add a marker in Sydney and move the camera
-        /*
-        LatLng uw = new LatLng(47.655548, -122.303200);
-        LatLng uw2 = new LatLng(47.655248, -122.303200);
-        LatLng uw3 = new LatLng(47.655548, -122.303000);
-        mMap.addMarker(new MarkerOptions().position(uw).title("Big bird!").snippet("Hey I saw a bald eagle over here come check it out!"));
-        mMap.addMarker(new MarkerOptions().position(uw2).title("Sunset!").snippet("Awesome view of the sunset from over here"));
-        mMap.addMarker(new MarkerOptions().position(uw3).title("Free Donuts!").snippet("I ordered too many donuts, who wants some?"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(uw));
-        */
-        Log.i("hello", "hello");
     }
 
     @Override
